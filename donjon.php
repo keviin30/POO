30f6bd02809b82ce84c3bc36d0112188aa9d061c
lisaadilia <lisa.da_cunha@devinci.fr>
2017-02-22 10:59:38 +0100

    La page donjon

---
+++ donjon.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Combat</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body style="background:url(img/wallpaper_by_arixev-d3eieg8.jpg); background-position: center center;
+background-repeat: no-repeat; background-attachment: fixed; background-size: cover; background-color: white;">
+
+
+    <div id="conteneur">
+        <div class="element"><img src="img/door.png"></div>
+        <div class="element"><img src="img/door.png"></div>
+        <div class="element"><img src="img/door.png"></div>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,26 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>Combat</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
-
-
-  <body style="background:url(img/fond.jpg); background-position: center center;
-  background-repeat: no-repeat; background-attachment: fixed; background-size: cover; background-color: white;">
-    <div class="container">
-      <div class="col-md-4">
-
-      </div>
-      <div class="col-md-4 ">
-        <div class="start">
-          <center><a href="donjon.php"><button type="button" class="btn btn-danger">Start The Game</button></a></center>
-        </div>
-      </div>
-      <div class="col-md-4">
-
-      </div>
+  <body>
+    <div class="start">
+      
     </div>
+
 
   </body>
 </html>
