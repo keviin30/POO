9660c6446830342e96a1ec31647ce4d493d276f0
Kevin CHAU <kevin.chau@devinci.fr>
2017-02-22 10:32:22 +0100

    commit de keke n°1

---
+++ gameover.php
@@ -0,0 +1,21 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 22/02/2017
+ * Time: 09:39
+ */
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Combat</title>
+    <link rel="stylesheet" href="css/gameover.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+
+  <body style="background:url(img/game.jpg); background-position: center center;
+background-repeat: no-repeat; background-attachment: fixed; background-size: cover; background-color: white;">
+  <img src="img/over.png" id="over">
+  </body>
+</html>
